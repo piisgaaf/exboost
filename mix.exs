3c096203a714721da9e11fa1058de171c209e1f2
Pieter Rijken <prijken@xebia.com>
2026-02-05 01:45:21 +0100

    simplified

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Exboost.MixProject do
   def project do
     [
       app: :exboost,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.19",
       start_permanent: false,
       build_embedded: Mix.env == :prod,
